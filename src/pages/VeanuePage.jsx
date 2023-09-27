1b4c8c6fb678250514fd6bce727a2bd9eafe2f33
grimoll2e <itmutcpe6@gmail.com>
2023-09-27 11:38:01 +0700

    create route

---
+++ src/pages/VeanuePage.jsx
@@ -0,0 +1,5 @@
+export default function VeanuePage() {
+    return (
+        <div>VeanuePage</div>
+    )
+}
---
+++ src/routes/Router.jsx
@@ -0,0 +1,48 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import LoginPage from "../pages/LoginPage";
+import SignupPage from "../pages/SignupPage";
+import HomePage from "../pages/HomePage";
+import AboutPage from "../pages/AboutPage";
+import VeanuePage from "../pages/VeanuePage";
+import EventPage from "../pages/EventPage";
+import ContactPage from "../pages/ContactPage";
+import SettingPage from "../pages/SettingPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/about",
+    element: <AboutPage />,
+  },
+  {
+    path: "/veanue",
+    element: <VeanuePage />,
+  },
+  {
+    path: "/event",
+    element: <EventPage />,
+  },
+  {
+    path: "/contact",
+    element: <ContactPage />,
+  },
+  {
+    path: "/setting",
+    element: <SettingPage />,
+  },
+]);
+
+export default function Router() {
+  return <RouterProvider router={router} />;
+}
